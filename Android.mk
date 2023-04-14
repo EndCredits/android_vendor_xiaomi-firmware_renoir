849bef4ce0d5ec1b8c2ca2a925c504fdba8d9688
EndCredits <endcredits@crepuscular-aosp.icu>
2023-04-15 01:30:15 +0800

    renoir-firmware: Initial from renoir CN V14.0.8.0
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

---
+++ Android.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2022 Paranoid Android
+# Copyright (C) 2022 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),renoir)
+
+$(info Including firmware for renoir...)
+
+FIRMWARE_IMAGES := $(wildcard $(LOCAL_PATH)/images/*)
+
+$(foreach f, $(notdir $(FIRMWARE_IMAGES)), \
+    $(call add-radio-file,images/$(f)))
+
+endif
